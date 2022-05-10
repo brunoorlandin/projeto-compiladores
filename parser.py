b20e1ca652efa2b54a18f007b6afd9861ab2decb
brunoorlandin <bruno.orlandin@hotmail.com>
2022-05-10 18:41:27 -0300

    finalizou attrib

--- parser.py
+++ parser.py
@@ -85,7 +85,6 @@ class Interpreter(object):
             self.eat("IDENTIFIER")
             if self.current_token.type == "ATRIBUIDOR":
                 self.eat("ATRIBUIDOR")
-                
                 if self.current_token.type == "IDENTIFIER":
                     self.eat("IDENTIFIER")
                     print("banana", self.current_token)
@@ -119,8 +118,7 @@ class Interpreter(object):
                             if self.current_token.type == "EOL":
                                 self.eat("EOL")
                                 status = self.expr()
-        
-       
+    
         if status == False:
             self.lexer.pos = pos
             self.current_token = token
@@ -130,9 +128,42 @@ class Interpreter(object):
     def attrib(self):
         """
         attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL | 
-                IDENTIFIER ATRIBUIDOR (INTEGER | SWORD) EOL |
-                IDENTIFIER ATRIBUIDOR (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL
+                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL
         """
+
+        print("entrou attrib")
+
+        if self.current_token.type == "IDENTIFIER":
+            self.eat("IDENTIFIER")
+            if self.current_token.type == "ATRIBUIDOR":
+                self.eat("ATRIBUIDOR")
+                if self.current_token.type == "IDENTIFIER":
+                    self.eat("IDENTIFIER")
+                    if self.current_token.type == "EOL":
+                        self.eat("EOL")
+                        status = self.expr()
+                elif self.current_token.type in ["INTEGER", "STRING"]:
+                    if self.current_token.type == "INTEGER":
+                        self.eat("INTEGER")
+                    elif self.current_token.type == "STRING":
+                        self.eat("STRING")
+                    if self.current_token.type == "EOL":
+                        self.eat("EOL")
+                        status = self.expr()
+
+
+        status = False
+
+        pos = self.lexer.pos
+        token = self.current_token
+
+
+
+        if status == False:
+            self.lexer.pos = pos
+            self.current_token = token
+
+        return status
 
 
     def expr(self):
@@ -141,10 +172,10 @@ class Interpreter(object):
         '''
         expr: vardec | basicop | attib | if-else | Ɛ
         vardec: (INTEGER | SWORD) IDENTIFIER EOL
-        basicop: INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL
+        basicop: IDENTIFIER ATRIBUIDOR IDENTIFIER (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL |
+                 IDENTIFIER ATRIBUIDOR INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL
         attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL | 
-                IDENTIFIER ATRIBUIDOR (INTEGER | SWORD) EOL |
-                IDENTIFIER ATRIBUIDOR (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL
+                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL
         if-else: HIT cond L_CHAVE S R_CHAVE MISS L_CHAVE S  R_CHAVE S | HIT cond L_CHAVE S R_CHAVE
         cond: IDENTIFIER comp IDENIFIER | NUMERIC comp NUMERIC | STRING DODGE STRING
         comp: ATTACK | DEFENSE | DODGE | CRITICAL | BLOCK 
@@ -156,6 +187,9 @@ class Interpreter(object):
             return True
         elif self.basicop():
             print("basicop")
+            return True
+        elif self.attrib():
+            print("attrib")
             return True
         elif self.current_token.type == 'EOF':
             return True

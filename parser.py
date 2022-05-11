e1b11430e3280cc02a0f8da3af6a9e0cf79305bb
brunoorlandin <bruno.orlandin@hotmail.com>
2022-05-11 10:28:20 -0300

    ajustes na logica do parser - add lista de tokens

--- parser.py
+++ parser.py
@@ -10,7 +10,21 @@ class Interpreter(object):
     def __init__(self, lexer):
         self.lexer = lexer
         # set current token to the first token taken from the input
-        self.current_token = self.lexer.get_next_token()
+        
+        self.ifQuantity = 0
+
+        self.listaTokens =[]
+        self.posListaTokens = 0
+
+        token = lexer.get_next_token()
+        self.current_token = token
+        while token.type != "EOF":
+          self.listaTokens.append(token)
+          token = lexer.get_next_token()
+        self.listaTokens.append(token)
+
+        print(self.listaTokens)
+
 
     def error(self):
         raise Exception('Invalid syntax')
@@ -21,63 +35,57 @@ class Interpreter(object):
         # and assign the next token to the self.current_token,
         # otherwise raise an exception.
         if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
+            self.posListaTokens += 1
+            self.current_token = self.listaTokens[self.posListaTokens]
         else:
             self.error()
 
     def vardec(self):
-        print("entrou vardec")
-        '''vardec: (INTEGER | SWORD) IDENTIFIER EOL'''
-        print(self.current_token)
-
+        print("Entrou VARDEC")
+        '''vardec: (SHIELD | SWORD) IDENTIFIER EOL EXPR'''
         status = False
+        pos = self.posListaTokens
 
-        pos = self.lexer.pos
-        token = self.current_token
 
         if self.current_token.type == "SHIELD":
             self.eat("SHIELD")
+
             if self.current_token.type == "IDENTIFIER":
                 self.eat("IDENTIFIER")
-                print(self.lexer.pos)
                 
                 if self.current_token.type == "EOL":
                     self.eat("EOL")
-                    print(self.lexer.pos)
+
                     status = self.expr()
+        
         elif self.current_token.type == "SWORD":
             self.eat("SWORD")
+
             if self.current_token.type == "IDENTIFIER":
                 self.eat("IDENTIFIER")
-                print(self.lexer.pos)
+
                 if self.current_token.type == "EOL":
                     self.eat("EOL")
-                    print(self.lexer.pos)
+
                     status = self.expr()
 
-        print(self.lexer.pos)
-        
-        
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de VARDEC ", status)
         return status
     
     def basicop(self):
-        print("entrou basicop")
+        print("Entrou BASICOP")
         """
-        basicop: IDENTIFIER ATRIBUIDOR IDENTIFIER (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL |
-                 IDENTIFIER ATRIBUIDOR INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL
+        basicop: IDENTIFIER ATRIBUIDOR IDENTIFIER (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL EXPR|
+                 IDENTIFIER ATRIBUIDOR INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL EXPR
         """
-        print(self.current_token)
-        
-        print(self.current_token.type)
 
         status = False
 
-        pos = self.lexer.pos
-        token = self.current_token
+        pos = self.posListaTokens
         
         op = ["BUFF", "DEBUFF", "HEAL", "POISON"]
 
@@ -87,7 +95,6 @@ class Interpreter(object):
                 self.eat("ATRIBUIDOR")
                 if self.current_token.type == "IDENTIFIER":
                     self.eat("IDENTIFIER")
-                    print("banana", self.current_token)
                     if self.current_token.type in op:
                         if self.current_token.type == "BUFF":
                             self.eat("BUFF")
@@ -120,166 +127,221 @@ class Interpreter(object):
                                 status = self.expr()
     
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de BASICOP ", status)
         return status
 
     def attrib(self):
+        print("Entrou ATTRIB")
+        
         """
-        attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL | 
-                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL
+        attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL EXPR | 
+                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL EXPR
         """
 
-        print("entrou attrib")
+        pos = self.posListaTokens
+
+        status = False
 
         if self.current_token.type == "IDENTIFIER":
             self.eat("IDENTIFIER")
+
             if self.current_token.type == "ATRIBUIDOR":
                 self.eat("ATRIBUIDOR")
+
                 if self.current_token.type == "IDENTIFIER":
                     self.eat("IDENTIFIER")
+
                     if self.current_token.type == "EOL":
                         self.eat("EOL")
+                        
                         status = self.expr()
-                elif self.current_token.type in ["INTEGER", "STRING"]:
+
+                
+                elif self.current_token.type in ("INTEGER", "STRING"):
                     if self.current_token.type == "INTEGER":
                         self.eat("INTEGER")
                     elif self.current_token.type == "STRING":
                         self.eat("STRING")
+
                     if self.current_token.type == "EOL":
                         self.eat("EOL")
                         status = self.expr()
-
-
-        status = False
-
-        pos = self.lexer.pos
-        token = self.current_token
-
-
-
+                
+        
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de ATTRIB ", status)
         return status
+        
 
     def ifElse(self):
+        print("Entrou ifELSE")
         '''
-        if-else: HIT cond L_CHAVE S R_CHAVE MISS L_CHAVE S  R_CHAVE S | 
-                 HIT cond L_CHAVE S R_CHAVE
+        if-else: HIT cond L_CHAVE EXPR R_CHAVE MISS L_CHAVE EXPR  R_CHAVE EXPR | 
+                 HIT cond L_CHAVE EXPR R_CHAVE EXPR
         '''
         status = False
+        pos = self.posListaTokens
 
-        pos = self.lexer.pos
-        token = self.current_token
+        if self.current_token.type == "HIT":
+            self.eat("HIT")
 
+            if self.cond():
+            
+                if self.current_token.type == "L_CHAVE":
+                    self.eat("L_CHAVE")
 
+                    self.ifQuantity += 1
 
+                    if self.expr():
+
+                        
+
+                        if self.current_token.type == "R_CHAVE":
+                            self.eat("R_CHAVE")
+                        
+                            if self.current_token.type == "MISS":
+                                self.eat("MISS")
+
+                                if self.current_token.type == "L_CHAVE":
+                                    self.eat("L_CHAVE")
+
+                                    self.ifQuantity += 1
+
+                                    if self.expr():
+
+                                        if self.current_token.type == "R_CHAVE":
+                                            self.eat("R_CHAVE")
+                                            status = self.expr()
+
+                            else:
+                                status = self.expr()
+        
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de ifElse ", status)
         return status
 
     def cond(self):
+        print("Entrou COND")
         '''
-        cond: IDENTIFIER comp IDENIFIER |
+        cond: IDENTIFIER comp IDENTIFIER |
               INTEGER comp INTEGER |
               STRING DODGE STRING
         '''
         status = False
+        pos = self.posListaTokens
 
-        pos = self.lexer.pos
-        token = self.current_token
 
-        
         if self.current_token.type == "IDENTIFIER":
             self.eat("IDENTIFIER")
-            status = self.comp()
-            if self.current_token.type == "IDENTIFIER":
-                self.eat("IDENTIFIER")
-                status = self.expr()
+            
+            if self.comp():
+                
+                if self.current_token.type == "IDENTIFIER":
+                    self.eat("IDENTIFIER")
 
+                    status = True
+        
+        elif self.current_token.type == "INTEGER":
+            self.eat("INTEGER")
+            
+            if self.comp():
+
+                if self.current_token.type == "INTEGER":
+                    self.eat("INTEGER")
+
+                    status = True
 
 
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de COND ", status)
         return status
 
     def comp(self):
+        print("Entrou COMP")
         '''
         comp: ATTACK | DEFENSE | DODGE | CRITICAL | BLOCK 
         '''
         status = False
+        pos = self.posListaTokens
 
-        pos = self.lexer.pos
-        token = self.current_token
+        if self.current_token.type in ("ATTACK", "DEFENSE", "DODGE", "CRITICAL", "BLOCK"):
+            status = True
+            self.eat(self.current_token.type)
 
-        if self.current_token.type == "ATTACK":
-            self.eat("ATTACK")
-            status = self.expr()
-        elif self.current_token.type == "DEFENSE":
-            self.eat("DEFENSE")
-            status = self.expr()
-        elif self.current_token.type == "DODGE":
-            self.eat("DODGE")
-            status = self.expr()
-        elif self.current_token.type == "CRITICAL":
-            self.eat("CRITICAL")
-            status = self.expr()
-        elif self.current_token.type == "BLOCK":
-            self.eat("BLOCK")
-            status = self.expr()
 
         if status == False:
-            self.lexer.pos = pos
-            self.current_token = token
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
 
+        print("Retorna de COMP ", status)
         return status
 
 
     def expr(self):
-        print(self.current_token)
+        #print(self.current_token)
+        print("Entrou EXPR")
+        pos = self.posListaTokens
 
         '''
         expr: vardec | basicop | attib | if-else | Ɛ
-        vardec: (INTEGER | SWORD) IDENTIFIER EOL
-        basicop: IDENTIFIER ATRIBUIDOR IDENTIFIER (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL |
-                 IDENTIFIER ATRIBUIDOR INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL
-        attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL | 
-                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL
-        if-else: HIT cond L_CHAVE S R_CHAVE MISS L_CHAVE S  R_CHAVE S | 
-                 HIT cond L_CHAVE S R_CHAVE
-        cond: IDENTIFIER comp IDENIFIER | NUMERIC comp NUMERIC | STRING DODGE STRING
+        vardec: (INTEGER | SWORD) IDENTIFIER EOL EXPR
+        basicop: IDENTIFIER ATRIBUIDOR IDENTIFIER (BUFF | DEBUFF | HEAL | POISON) IDENTIFIER EOL EXPR|
+                 IDENTIFIER ATRIBUIDOR INTEGER (BUFF | DEBUFF | HEAL | POISON) INTEGER EOL EXPR
+        attrib: IDENTIFIER ATRIBUIDOR IDENTIFIER EOL EXPR | 
+                IDENTIFIER ATRIBUIDOR (INTEGER | STRING) EOL EXPR
+        if-else: HIT cond L_CHAVE EXPR R_CHAVE MISS L_CHAVE EXPR R_CHAVE EXPR | 
+                 HIT cond L_CHAVE EXPR R_CHAVE EXPR
+        cond: IDENTIFIER comp IDENTIFIER | NUMERIC comp NUMERIC | STRING DODGE STRING
         comp: ATTACK | DEFENSE | DODGE | CRITICAL | BLOCK 
         '''
 
-
+        
+        
+        
         if self.vardec():
-            print("vardec")
+            print("Return EXPR True")
             return True
+        
         elif self.basicop():
-            print("basicop")
+            print("Return EXPR True")
             return True
+        
         elif self.attrib():
-            print("attrib")
+            print("Return EXPR True")
             return True
-        elif self.cond():
-            print("cond")
+
+        elif self.ifElse():
+            print("Return EXPR True")
             return True
-        elif self.comp():
-            print("comp")
-            return True
+        
         elif self.current_token.type == 'EOF':
+            print("Return EXPR True")
+            return True
+
+        elif self.current_token.type == 'R_CHAVE' and self.ifQuantity > 0:
+            self.ifQuantity -= 1
+            print("Return EXPR True")
             return True
         else:
-            print(self.current_token)
+
+            print("Return EXPR False")
+            self.posListaTokens = pos
+            self.current_token = self.listaTokens[pos]
             self.error()
             return False
+
         
 
 def main():
@@ -295,11 +357,13 @@ def main():
         if lines_split[i][0] != '':
             lines.append(lines_split[i][0])
 
+    text = ""
     for i in range(len(lines)):
-        text = lines[i]
-        lexer = Lexer(text)
-        interpreter = Interpreter(lexer)
-        result = interpreter.expr()
+        text += lines[i]
+
+    lexer = Lexer(text)
+    interpreter = Interpreter(lexer)
+    result = interpreter.expr()
 
     print(result)
 
